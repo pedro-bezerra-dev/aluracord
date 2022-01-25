eca8daeba8d0e67edb2638a0993e06062dc4932d
Pedro Henrique <pyter.dev@gmail.com>
2022-01-25 12:10:58 -0300

    Theme added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,11 @@
-const theme = {}
+const theme = {
+  colors: {
+    primary: '#5dfddd',
+    secondary: '#0a151f',
+    tertiary: '#1a2e40',
+    quaternary: '#050b10',
+    quintenary: '#cce2f5',
+  }
+}
 
 export default theme
