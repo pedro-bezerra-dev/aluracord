1eac6cf2102366cda6411d8caf6315147115f0d4
Pedro Henrique <pyter.dev@gmail.com>
2022-01-26 10:28:28 -0300

    Font styles added to GlobalStyles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -6,6 +6,18 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  .title {
+    font: 700 36px 'Outfit', sans-serif;
+  }
+  .highlighted-light {
+    font: 400 18px 'Outfit', sans-serif;
+  }
+  .highlighted-bold {
+    font: 700 18px 'Outfit', sans-serif;
+  }
+  .body {
+    font: 400 18px 'Open Sans', sans-serif;
+  }
 `
 
 export default GlobalStyles
