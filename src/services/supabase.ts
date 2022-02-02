2c8d4583f4ac1ec9b7548b7eafeaa988fd0ed2f4
Pedro Henrique <pyter.dev@gmail.com>
2022-02-01 21:49:58 -0300

    Supabase client is now exported from supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -15,7 +15,12 @@ type SubscribeForChangesProps = {
   callbackForChanges: (payload:SupabaseRealtimePayload<any>) => void;
 }
 
-export async function getAllMessages(supabaseClient:SupabaseClient) {
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY : ''
+
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey)
+
+export async function getAllMessages() {
   let messages: Messages = []
 
   try {
